696ce3acb291f0314a6e7131c4abc34975cb4ef4
BurcuHaddad <burcuhaddad@gmail.com>
2023-10-27 11:13:49 +0300

    hashing example and explanation

---
+++ src/dev/bh/Playingcard.java
@@ -0,0 +1,36 @@
+package dev.bh;
+
+public class Playingcard {
+    private String suit;
+    private String face;
+    private int internalHash;
+
+    public Playingcard(String suit, String face) {
+        this.suit = suit;
+        this.face = face;
+        this.internalHash = (suit.equals("Hearts")) ? 11 : 12;
+    }
+
+    @Override
+    public String toString() {
+        return face + " of " + suit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Playingcard that = (Playingcard) o;
+
+        if (!suit.equals(that.suit)) return false;
+        return face.equals(that.face);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = suit.hashCode();
+        result = 31 * result + face.hashCode();
+        return result;
+    }
+}
